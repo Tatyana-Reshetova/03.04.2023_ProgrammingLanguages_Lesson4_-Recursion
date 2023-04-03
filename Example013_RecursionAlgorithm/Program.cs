5529678a0829a5b91aaa74f8c33720f1df8fb87a
Tatyana_Reshetova <vlasenko2205@gmail.com>
2023-04-03 17:13:37 +0300

    Числа Фибоначчи

--- Example013_RecursionAlgorithm/Program.cs
+++ Example013_RecursionAlgorithm/Program.cs
@@ -24,31 +24,50 @@
 //    Console.WriteLine();
 //}
 
-void PrintArray(int[,] matr)
-{
-    for ( int i = 0; i < matr.GetLength(0); i++)
-    {
-        for ( int j = 0; j < matr.GetLength(1); j++)
-        {
-            Console.Write($"{matr[i,j]} ");
-        }
-        Console.WriteLine();
-    }
-}
+//void PrintArray(int[,] matr)
+//{
+//    for ( int i = 0; i < matr.GetLength(0); i++)
+//    {
+//        for ( int j = 0; j < matr.GetLength(1); j++)
+//        {
+//            Console.Write($"{matr[i,j]} ");
+//        }
+//        Console.WriteLine();
+//    }
+//}
 
-void FillArray (int[,] matr)
-{
-    for ( int i = 0; i < matr.GetLength(0); i++)
-    {
-        for ( int j = 0; j < matr.GetLength(1); j++)
-        {
-            matr[i,j] = new Random ().Next(1,10);
-        }
-    }
-}
-int[,] matr = new int[3,4];
+//void FillArray (int[,] matr)
+//{
+//    for ( int i = 0; i < matr.GetLength(0); i++)
+//    {
+//        for ( int j = 0; j < matr.GetLength(1); j++)
+//        {
+//            matr[i,j] = new Random ().Next(1,10);
+//        }
+//    }
+//}
+//int[,] matr = new int[3,4];
 
-PrintArray(matr);
-FillArray(matr);
-Console.WriteLine();
-PrintArray(matr);
+//PrintArray(matr);
+//FillArray(matr);
+//Console.WriteLine();
+//PrintArray(matr);
+
+//int Factorial(int n)
+//{
+//    //1! = 1
+//    //0! = 1
+//    if (n == 1) return 1;
+//    else return n * Factorial(n-1);
+//}
+//Console.WriteLine(Factorial(3));
+
+int Fibonacci(int n)
+{
+if ( n == 1 || n == 2) return 1;
+else return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+for (int i = 1; i < 10; i++)
+{
+    Console.WriteLine(Fibonacci(i));
+}
